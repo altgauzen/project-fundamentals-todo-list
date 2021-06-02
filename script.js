af0ab3f7a7573e565e95310d1f6805847647039b
Alexandre Altgauzen <altgauzen@gmail.com>
2021-06-02 22:53:08 +0100

    requisitos refatorados ate o 5

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function enterText() {
-  let text = document.getElementById("texto-tarefa").value;
-  document.getElementById("lista-tarefas").innerHTML = text;
+/*function enterText() {
+  let text = document.getElementById('texto-tarefa').value;
+  document.getElementById('lista-tarefas').innerHTML = text;
+  document.getElementById('texto-tarefa').value = '';
+  newListItem();
+}*/
+
+function newListItem() {
+  let li = document.createElement('li');
+  let inputValue = document.getElementById('texto-tarefa').value;
+  let t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  document.getElementById('lista-tarefas').appendChild(li);
+  document.getElementById('texto-tarefa').value = '';
 }
