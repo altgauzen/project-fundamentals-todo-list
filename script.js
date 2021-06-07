309fd5106269f764c6db16b48d03e77488f94fe6
Alexandre Altgauzen <altgauzen@gmail.com>
2021-06-07 19:15:34 +0100

    requisitos 9

--- script.js
+++ script.js
@@ -18,5 +18,15 @@ function newListItem() {
   li.appendChild(t);
   document.getElementById('lista-tarefas').appendChild(li);
   li.addEventListener('click', changeColorItem);
+  li.addEventListener('dblclick', doubleClick);
   document.getElementById('texto-tarefa').value = '';
+}
+
+function doubleClick(event) {
+  let lineSelected = event.target;
+  if (lineSelected.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
 }
