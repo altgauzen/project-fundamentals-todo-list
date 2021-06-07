24bc8b89b43d581d1871d0d711407e1b50970081
Alexandre Altgauzen <altgauzen@gmail.com>
2021-06-07 17:54:42 +0100

    requisitos 7 refatorado novamente

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-/*window.onload = initPage;
-
-function initPage() {
-  if (document.getElementById('lista-tarefas').style.backgroundColor === null) {
-    return = 'Os itens da lista não tem o estilo CSS backgroun color'
-  }
-}*/
-
 function newListItem() {
-  const li = document.createElement('li');
-  const inputValue = document.getElementById('texto-tarefa').value;
-  const t = document.createTextNode(inputValue);
+  let li = document.createElement('li');
+  let inputValue = document.getElementById('texto-tarefa').value;
+  let t = document.createTextNode(inputValue);
   li.appendChild(t);
   document.getElementById('lista-tarefas').appendChild(li);
-  document.getElementById('lista-tarefas').style.backgroundColor = 'rgb(128,128,128)';
+  li.addEventListener('click',changeColorItem);
   document.getElementById('texto-tarefa').value = '';
+}
+
+function changeColorItem(event) { 
+  let allLines = document.querySelectorAll('li');
+  let lineSelected = event.target;
+  for (let index = 0; index < allLines.length; index += 1) {
+    let currentLine = allLines[index];
+    if (currentLine === lineSelected) {
+      event.target.classList.add('selected');
+    } else {
+      currentLine.classList.remove('selected');
+    }
+  }
 }
