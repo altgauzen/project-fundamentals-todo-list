0eadf22ddcec7ffc2982962453f950a9e91afe9b
Alexandre Altgauzen <altgauzen@gmail.com>
2021-07-07 14:44:24 +0100

    css e js refatorados

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function changeColorItem(event) {
-  let allLines = document.querySelectorAll('li');
-  let lineSelected = event.target;
+  //  armazena na constante allLines a lista dos elementos li 
+  const allLines = document.querySelectorAll('li');
+  //  armazena na constante lineSelected o elemento acionado no click
+  const lineSelected = event.target;
   for (let index = 0; index < allLines.length; index += 1) {
-    let currentLine = allLines[index];
+    const currentLine = allLines[index];
+    //  se condição atendida, adicionar a classe selected ao item da lista
     if (currentLine === lineSelected) {
       event.target.classList.add('selected');
     } else {
@@ -10,8 +13,8 @@ function changeColorItem(event) {
     }
   }
 }
-
-function doubleClick(event) {
+//  background-color: rgb(128, 128, 128);
+function lineThrough(event) {
   let lineSelected = event.target;
   if (lineSelected.classList.contains('completed')) {
     event.target.classList.remove('completed');
@@ -21,13 +24,22 @@ function doubleClick(event) {
 }
 
 function newListItem() {
+  //  cria um item de lista e armazena na constante li
   const li = document.createElement('li');
+  //  busca o valor inserido no input texto-tarefa e armazena na constante inputValue
   const inputValue = document.getElementById('texto-tarefa').value;
+  //  armazena o inputValue na constante t um text node criado segundo referencia no link abaixo
+  // https://www.w3schools.com/jsref/met_document_createtextnode.asp
   const t = document.createTextNode(inputValue);
+  //  anexa o texto do text node t ao item da lista como último filho
   li.appendChild(t);
+  //  anexa a constante li como última filha do elemento lista ordenada capturado no dom pelo id lista-tarefas
   document.getElementById('lista-tarefas').appendChild(li);
+  //  adiciona escutador de click, para chamar função da mudança de cor
   li.addEventListener('click', changeColorItem);
-  li.addEventListener('dblclick', doubleClick);
+  //  adicionado escutador de duplo click, para chamar função da tarefa completa
+  li.addEventListener('dblclick', lineThrough);
+  //  limpa o texto digitado no input ao termino da função (recebe valor limpo)
   document.getElementById('texto-tarefa').value = '';
 }
 
